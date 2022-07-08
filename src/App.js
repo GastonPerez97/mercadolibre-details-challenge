f9fa1c2e21f6367a8a38726859fbb62a7cd1dd75
Gastón Pérez <gastonperez97@gmail.com>
2022-07-08 09:37:37 -0300

    sweetalert2 dependency, warning modal on start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import { useState } from "react";
-import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import Breadcrumbs from "./components/Breadcrumbs/Breadcrumbs";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import ProductCarousel from "./components/ProductCarousel/ProductCarousel";
 
+import Swal from "sweetalert2";
+
+import "./App.css";
+
 const App = () => {
 	const [darkMode, setDarkMode] = useState(false);
 
 	const toggleDarkMode = () => setDarkMode(prevState => !prevState);
 
+	Swal.fire({
+		title: "¡Atención!",
+		text: "Esta web NO es el sitio oficial de Mercado Libre, se trata de un challenge de Frontend, no es un intento de phishing hacia los usuarios que la visitan.",
+		icon: "warning",
+		confirmButtonText: "¡Entendido!",
+		confirmButtonColor: "#3484fa",
+		backdrop: "rgba(0, 0, 0, 0.8)",
+	});
+
 	return (
 		<>
 			<header className={darkMode ? "dark-mode" : ""}>
-				<Navbar
-					darkMode={ darkMode }
-					toggleDarkMode={ toggleDarkMode }
-				/>
+				<Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
 			</header>
 
 			<main className={darkMode ? "dark-mode" : ""}>
@@ -28,21 +37,28 @@ const App = () => {
 
 			<footer className={darkMode ? "dark-mode" : ""}>
 				Desarrollado por&nbsp;
-				<a href="https://gastonperez.ar/" target="_blank" rel="noreferrer">
+				<a
+					href="https://gastonperez.ar/"
+					target="_blank"
+					rel="noreferrer"
+				>
 					Gast&oacute;n P&eacute;rez
 				</a>
-
 				<p>
 					ATENCIÓN: Esta web se trata de un&nbsp;
-						<a
-							href="https://github.com/goncy/mercadolibre-details-challenge"
-							target="_blank"
-							rel="noreferrer"
-						>challenge</a>, no es un intento de phishing hacia los usuarios que la visitan.
+					<a
+						href="https://github.com/goncy/mercadolibre-details-challenge"
+						target="_blank"
+						rel="noreferrer"
+					>
+						challenge
+					</a>
+					, no es un intento de phishing hacia los usuarios que la
+					visitan.
 				</p>
 			</footer>
 		</>
 	);
-}
+};
 
 export default App;
